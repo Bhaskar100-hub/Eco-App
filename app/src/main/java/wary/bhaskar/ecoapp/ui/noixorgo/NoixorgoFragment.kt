1782a61a942505e9399a6215ce0782cc18e313ab
Bhaskar100-hub <bhaskarwary7@gmail.com>
2022-06-15 02:23:50 +0530

    Chnaged some color in Side Navigation Drawer

--- app/src/main/java/wary/bhaskar/ecoapp/ui/noixorgo/NoixorgoFragment.kt
+++ app/src/main/java/wary/bhaskar/ecoapp/ui/noixorgo/NoixorgoFragment.kt
@@ -7,37 +7,18 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import wary.bhaskar.ecoapp.R
 import wary.bhaskar.ecoapp.databinding.FragmentNoixorgoBinding
 import wary.bhaskar.ecoapp.ui.noixorgo.NoixorgoViewModel
 
 class NoixorgoFragment : Fragment() {
-
-    private var _binding: FragmentNoixorgoBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val noixorgoViewModel =
-            ViewModelProvider(this).get(NoixorgoViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_noixorgo, container, false)
 
-        _binding = FragmentNoixorgoBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textNoixorgo
-        noixorgoViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
