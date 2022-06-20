41f98ce7ed81b6c1a7fad6d48e9f42a05aa6e0f6
Bhaskar100-hub <bhaskarwary7@gmail.com>
2022-06-20 10:54:50 +0530

    Team section is working wine after adding toggle button

--- app/src/main/java/wary/bhaskar/ecoapp/ui/team/TeamFragment.kt
+++ app/src/main/java/wary/bhaskar/ecoapp/ui/team/TeamFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_team.*
 import wary.bhaskar.ecoapp.R
 
 class TeamFragment : Fragment() {
@@ -15,5 +17,22 @@ class TeamFragment : Fragment() {
     ): View {
         val root = inflater.inflate(R.layout.fragment_team, container, false)
         return root
+        toggleButtonGroup.addOnButtonCheckedListener { toggleButtonGroup, checkedId, isChecked ->
+
+            if (isChecked) {
+                when (checkedId) {
+                    R.id.btnAndroid -> showToast("Robot that looks like human.")
+                    R.id.btnFlutter -> showToast("It's a Butterfly thing.")
+                    R.id.btnWeb -> showToast("You still exist?")
+                }
+            } else {
+                if (toggleButtonGroup.checkedButtonId == View.NO_ID) {
+                    showToast("Nothing Selected")
+                }
+            }
+        }
+    }
+    private fun showToast(str: String) {
+        Toast.makeText(getContext(), str, Toast.LENGTH_LONG).show()
     }
 }
