9957373a999703aeb12938c8f460fb774872ea01
Bhaskar100-hub <bhaskarwary7@gmail.com>
2022-06-20 12:25:08 +0530

    scrollable team layout

--- app/src/main/java/wary/bhaskar/ecoapp/ui/team/TeamFragment.kt
+++ app/src/main/java/wary/bhaskar/ecoapp/ui/team/TeamFragment.kt
@@ -17,22 +17,5 @@ class TeamFragment : Fragment() {
     ): View {
         val root = inflater.inflate(R.layout.fragment_team, container, false)
         return root
-        toggleButtonGroup.addOnButtonCheckedListener { toggleButtonGroup, checkedId, isChecked ->
-
-            if (isChecked) {
-                when (checkedId) {
-                    R.id.btnAndroid -> showToast("Robot that looks like human.")
-                    R.id.btnFlutter -> showToast("It's a Butterfly thing.")
-                    R.id.btnWeb -> showToast("You still exist?")
-                }
-            } else {
-                if (toggleButtonGroup.checkedButtonId == View.NO_ID) {
-                    showToast("Nothing Selected")
-                }
-            }
-        }
-    }
-    private fun showToast(str: String) {
-        Toast.makeText(getContext(), str, Toast.LENGTH_LONG).show()
     }
 }
